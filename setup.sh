21997eb054e1711244a86b529c970a2f74de56e4
Jared Sampson <jared.sampson@gmail.com>
2023-04-11 22:08:37 -0400

    Add ask_for_sudo function.

--- setup.sh
+++ setup.sh
@@ -24,6 +24,19 @@ function print_step() {
 }
 
 
+ask_for_sudo() {
+    # Ask for the administrator password upfront.
+    sudo -v &> /dev/null
+
+    # Update existing `sudo` time stamp until this script has finished.
+    # https://gist.github.com/cowboy/3118588
+    while true; do
+        sudo -n true
+        sleep 60
+        kill -0 "$$" || exit
+    done &> /dev/null &
+}
+
 
 
 ############################################################
@@ -137,6 +150,9 @@ function main() {
     cd "$(dirname "${BASH_SOURCE[0]}")" \
         || exit 1
     print_step "Starting in $(pwd)..."
+
+    # Get sudo permission at the start.
+    ask_for_sudo
 
     # Download the repo if we're not running from a local copy
     if ! work_dir_has_setup_script; then
