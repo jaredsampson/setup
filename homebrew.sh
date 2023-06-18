3797bea6c3a8ecadfa14619532492d10d4a80cef
Jared Sampson <jared.sampson@gmail.com>
2023-06-17 21:56:36 -0400

    Create homebrew.sh

---
+++ homebrew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+install_homebrew() {
+    if ! cmd_exists "brew"; then
+        printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
+        #  └─ simulate the ENTER keypress
+    fi
+}
+
+brew update
